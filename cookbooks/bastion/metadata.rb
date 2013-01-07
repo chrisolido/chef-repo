5a62f699b114d52d0690824b9aef1020b18283fb
chrisolido <chris.olido@gmail.com>
2013-01-07 07:53:20 +0000

    Added hello bastion to the default.rb recipe

---
+++ cookbooks/bastion/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Fastacash"
+maintainer_email "chris.olido@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures bastion"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "bastion::default","Prints hello bastion"
---
+++ cookbooks/bastion/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: bastion
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello bastion here!"
